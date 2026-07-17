a646bd30e20c46af54dd95a612d04ac18779fa0a
Karan Devkar <devkarkaran130@gmail.com>
2026-07-17 10:50:59 +0530

    made academics page better

--- assets/js/academics.js
+++ assets/js/academics.js
@@ -38,6 +38,29 @@ addSubjectBtn.addEventListener("click", function(){
 function displaySubjects(){
 
     subjectsContainer.innerHTML = "";
+    // Update Statistics Cards
+
+document.getElementById("subjectCount").textContent = subjects.length;
+
+if(subjects.length > 0){
+
+    let total = 0;
+
+    subjects.forEach(function(subject){
+
+        total += Number(subject.marks);
+
+    });
+
+    let average = Math.round(total / subjects.length);
+
+    document.getElementById("averageMarks").textContent = average + "%";
+
+}else{
+
+    document.getElementById("averageMarks").textContent = "0%";
+
+}
 
     subjects.forEach(function(subject,index){
 
@@ -46,14 +69,55 @@ function displaySubjects(){
         div.className = "subject";
 
         div.innerHTML = `
-            <span>📘 ${subject.name}</span>
+    <div class="subject-left">
 
-            <span>${subject.marks}%</span>
+        <div class="subject-icon">
+            📚
+        </div>
 
-            <button onclick="deleteSubject(${index})">
-                🗑
-            </button>
-        `;
+        <div class="subject-name">
+            ${subject.name}
+        </div>
+
+    </div>
+
+    <div class="subject-percent">
+
+        <div class="percent-text">
+            ${subject.marks}%
+        </div>
+
+        <div class="progress">
+
+            <div
+                class="progress-fill"
+                style="width:${subject.marks}%">
+            </div>
+
+        </div>
+
+        <div class="subject-status">
+
+            ${
+                subject.marks >= 90
+                ? "🏆 Excellent! Outstanding Performance"
+                : subject.marks >= 75
+                ? "💪 Great Job! Keep it up"
+                : subject.marks >= 60
+                ? "👍 Good! Can improve more"
+                : subject.marks >= 40
+                ? "📚 Needs More Practice"
+                : "⚠️ Focus on this Subject"
+            }
+
+        </div>
+
+    </div>
+
+    <button class="delete-btn" onclick="deleteSubject(${index})">
+        🗑 Delete
+    </button>
+`;
 
         subjectsContainer.appendChild(div);
 
